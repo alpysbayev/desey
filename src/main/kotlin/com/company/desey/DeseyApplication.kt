dd871803b54ed4ffc887473347091473c6f3864f
Alpysbayev Adilet Kanagatuly <adilet.alpysbayev@gmail.com>
2023-01-15 22:49:35 +0600

    first commit

---
+++ src/main/kotlin/com/company/desey/DeseyApplication.kt
@@ -0,0 +1,11 @@
+package com.company.desey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeseyApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeseyApplication>(*args)
+}
